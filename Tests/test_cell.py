d195413888f6b19b504885f94ce7b4d23f96910c
TheBrodsky <stephensbrod@gmail.com>
2020-12-19 16:31:23 -0800

    Updated test files
    These were previously committed to the base directory when they should have been in Test/

--- Tests/test_cell.py
+++ Tests/test_cell.py
@@ -4,10 +4,14 @@ from cell import Cell
 
 class CellTests(ut.TestCase):
     def setUp(self):
-        self.cell = Cell()
+        self.cell = Cell(Cell.CellType.EMPTY)
 
-    def test_EmptyCellInitialization(self):
+    def test_CellInitializesToCorrectType(self):
         self.assertEqual(self.cell.type, Cell.CellType.EMPTY)
+        self.cell = Cell(Cell.CellType.BLACK)
+        self.assertEqual(self.cell.type, Cell.CellType.BLACK)
+        self.cell = Cell(Cell.CellType.RED)
+        self.assertEqual(self.cell.type, Cell.CellType.RED)
 
     def test_CellCanChangeType(self):
         self.cell.set_type(Cell.CellType.RED)
@@ -23,6 +27,7 @@ class CellTests(ut.TestCase):
         self.assertEqual(str(self.cell), "R")
         self.cell.set_type(Cell.CellType.BLACK)
         self.assertEqual(str(self.cell), "B")
+
 
 if __name__ == "__main__":
     ut.main()
